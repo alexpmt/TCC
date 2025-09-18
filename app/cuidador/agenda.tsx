fccdc5894746f2c4c6ffe9d08294ec0155d593cf
alexpmt <103111203+alexpmt@users.noreply.github.com>
2025-09-18 17:53:03 -0300

    Update agenda.tsx

---
+++ app/cuidador/agenda.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AgendaCuidador() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Agenda de Medicações</Text>
+    </View>
+  );
+}
