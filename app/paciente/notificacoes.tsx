5117adc1d40423ab560a4a8092f6231644b7b0fb
alexpmt <103111203+alexpmt@users.noreply.github.com>
2025-09-18 17:55:53 -0300

    Update notificacoes.tsx

---
+++ app/paciente/notificacoes.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function NotificacoesPaciente() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Notificações do Paciente</Text>
+    </View>
+  );
+}
