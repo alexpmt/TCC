53192a40a1b52c8dac79c1e816e9d84ba154ef72
alexpmt <103111203+alexpmt@users.noreply.github.com>
2025-09-18 17:55:42 -0300

    Update historico.tsx

---
+++ app/paciente/historico.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HistoricoPaciente() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Histórico de Medicações</Text>
+    </View>
+  );
+}
